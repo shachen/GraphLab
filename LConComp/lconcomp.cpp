0f0f5e9907a47cf676ce688c361f60d212e171a4
Shaojie CHEN <shachen@jhsph.edu>
2013-11-21 16:35:22 -0500

    renamed files

---
+++ LConComp/lconcomp.cpp
@@ -0,0 +1,10 @@
+# include <graphlab.hpp>
+
+
+int main(int argc, char** argv) {
+	std::count << "Largest Connected Component\n\n";
+	
+	graphlab::mpi_tools::init(argc, argv);
+	graphlab::distributed_control dc;
+
+}
